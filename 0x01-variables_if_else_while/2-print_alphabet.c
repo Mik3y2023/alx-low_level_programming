df3987bacf6412055f275275be94776580c42206
Mik3y2023 <oloomike77@gmail.com>
2023-03-21 06:20:56 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/** main - main entry point
+ *
+ * return: Always return 0 (success)
+ */
+
+int main (void)
+{
+	char c;
+	c = 'a';
+	while (c = 'a'){
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return 0;
+}
