4079bb6c68efcefa76a1aaf7b6030152843c9a6a
Mik3y2023 <oloomike77@gmail.com>
2023-03-21 11:41:38 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This is the main entry point
+ *
+ * return: always return 0 (success)
+ */
+
+int main(void)
+{
+int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
