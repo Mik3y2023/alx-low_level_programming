05df106b56ee5e2a5ccfcb1eb2799eca7abe3e9f
Mik3y2023 <oloomike77@gmail.com>
2023-03-21 11:50:07 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - This is the main entry point
- *
- * return: always 0 (success)
+ * main - Entry point
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-int c;
+	int c;
 
 	c = 0;
 	while
