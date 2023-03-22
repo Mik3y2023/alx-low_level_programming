8e9bf6239776ff1c6f5986829be576dbf76dcf28
Mik3y2023 <oloomike77@gmail.com>
2023-03-22 14:52:51 -0700

    isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - check for alphabetic order
  * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * Return: 1 if g is a letter, 0 otherwise
  */
 int _isalpha(int g)
 {
