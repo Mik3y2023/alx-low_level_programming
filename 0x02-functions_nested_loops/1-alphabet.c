037caecf2226b107f5681e74c255301cd02ed0ec
Mik3y2023 <oloomike77@gmail.com>
2023-03-22 13:38:06 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,14 @@
 /**
  * print_alphabet - prints the alphabet in lowercase
  *
- * return: Always 0 Success
+ * return: 0 Success
  */
 
 void print_alphabet(void);
 {
 	char c;
-	for (c='a', c<='z', c++)
-		putchar (c);
-	return 0;
+	for (c='a', c<='z', c++){
+		_putchar(c);
+	}
+	_putchar("\n");
 }
