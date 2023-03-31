61045ec1f7efd5d25b4b023f3ccc81d42680a4b4
Mik3y2023 <oloomike77@gmail.com>
2023-03-31 11:32:47 -0700

    strcmp

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,11 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+int _putchar(char c);
 
 #endif
