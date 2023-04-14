f1f975d19a4d16f6e19a6e4696c9877ef65bcb31
Mik3y2023 <oloomike77@gmail.com>
2023-04-14 02:43:50 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments followed by a new line
+ * @argc: number of arguments in a command line
+ * @argv: array containing arguments in a command line
+ * Return: 0 success
+ */
+
+int main( int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
