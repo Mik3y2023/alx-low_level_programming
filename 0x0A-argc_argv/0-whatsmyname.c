9582df5b5be8da42235b9ba079ee2525bffd9e45
Mik3y2023 <oloomike77@gmail.com>
2023-04-14 02:09:55 -0700

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints it's name followed by a new line
+ * @argc: number of command line arguments
+ * @argv: array containing command line arguments
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1){
+		printf("%s\n", argv[0]);
+	}
+	return 0;
+}
