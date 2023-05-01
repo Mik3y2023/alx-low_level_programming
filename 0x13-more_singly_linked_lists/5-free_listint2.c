53f738a58633a285c778450b6c3426b9abc8460e
Mik3y2023 <oloomike77@gmail.com>
2023-05-01 11:17:18 -0700

    small edits

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,18 +3,19 @@
 /**
  * free_listint2 - a function that frees a listint_t list
  * @head: the pointer to the head of the node
+ *
  * Return: nothing
  */
 
 void free_listint2(listint_t **head)
 {
-	listint_t *abc;
+	listint_t *temp_node;
 
 	while (*head)
 	{
-		abc = (*head)->next;
+		temp_node = (*head)->next;
 		free(*head);
-		*head = abc;
+		*head = temp_node;
 	}
 
 	*head = NULL;
