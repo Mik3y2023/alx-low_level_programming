f0497529f1756e3538ca8e16e04cd0c4f8cc3dc9
Michael <oloomike77@gmail.com>
2023-08-25 02:33:29 -0700

    gn

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,52 @@
+/**
+ * add - function that adds 2 intergers
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subraction
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulus
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
